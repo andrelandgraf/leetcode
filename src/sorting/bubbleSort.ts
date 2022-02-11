1423c6bbdc2dfe16206e5ad2229cc5500c889ab6
Andre Landgraf <andre.timo.landgraf@gmail.com>
2022-02-11 00:21:46 -0800

    Optimized Bubble Sort 💭

--- src/sorting/bubbleSort.ts
+++ src/sorting/bubbleSort.ts
@@ -3,24 +3,19 @@
  * Space complexity of O(1) (sorting in place)
  */
 function bubbleSort(array: number[]): number[] {
-    let i = 0;
-    let hasBubbled = false;
-    while (i < array.length - 1) {
-        const current = array[i];
-        const next = array[i + 1];
-        if (current > next) {
-            array[i] = next;
-            array[i + 1] = current;
-            hasBubbled = true;
-        }
-        i += 1;
-        if (i + 1 === array.length) {
-            if (hasBubbled) {
-                i = 0;
-                hasBubbled = false;
-            } else {
-                return array;
+    for (let i = 0; i < array.length; i += 1) {
+        let hasBubbled = false;
+        for (let j = 0; j < array.length - i - 1; j += 1) {
+            const current = array[j];
+            const next = array[j + 1];
+            if (current > next) {
+                array[j] = next;
+                array[j + 1] = current;
+                hasBubbled = true;
             }
+        }
+        if (!hasBubbled) {
+            return array;
         }
     }
     return array;
