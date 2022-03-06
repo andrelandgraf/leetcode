83b73d99a6b8132aaf5c27f5884fe24cee56d83a
Andre Landgraf <andre.timo.landgraf@gmail.com>
2022-03-06 13:16:10 -0800

    Optimizing with Maps 🗺️

--- src/two-sum/README.md
+++ src/two-sum/README.md
@@ -9,3 +9,6 @@ From: leetcode.com: https://leetcode.com/problems/two-sum/
 -   Results from leetcode.com (sort + traverse + traverse implementation)
     -   Runtime: 76 ms, faster than 93.69% of TypeScript online submissions for Two Sum.
     -   Memory Usage: 45.5 MB, less than 22.66% of TypeScript online submissions for Two Sum.
+-   Results from leetcode.com (hasmap + traverse + find implementation)
+    -   Runtime: 94 ms, faster than 80.80% of TypeScript online submissions for Two Sum.
+    -   Memory Usage: 48.6 MB, less than 5.03% of TypeScript online submissions for Two Sum.
--- src/two-sum/twoSum.ts
+++ src/two-sum/twoSum.ts
@@ -3,42 +3,33 @@
  * Brute force (two nested loops):
  *  - Time complexity: O(N^2)
  *  - Space complexity: O(1)
- * Performant alternative:
- *  - Time complexity: Sort O(NlogN) + Traverse O(N) + Traverse O(N) => O(NlogN)
- *  - Space complexity: O(N) since we create a sorted copy
+ * Performant alternatives:
+ *  - Sort O(NlogN) and Traverse once O(N) from both sides => O(NlogN)
+ * Even better:
+ *  - Time complexity: Traverse O(N) + Traverse O(N) => O(N)
+ *  - Space complexity: O(N) since we create a map including all elements
  *
  * @returns {Array} the tuple of indexes based on the unsorted input
  */
 function twoSum(nums: number[], target: number): number[] {
-    if (nums.length < 2) {
-        return [];
+    const values: Map<number, number[]> = new Map();
+    // O(N)
+    for (let i = 0; i < nums.length; i += 1) {
+        let curr = values.get(nums[i]);
+        if (!curr) {
+            curr = [];
+        }
+        curr.push(i);
+        values.set(nums[i], curr);
     }
-    const sortedNums = [...nums].sort((a, b) => a - b); // copy required to remember old indices
-    let left = 0;
-    let right = sortedNums.length - 1;
-    while (left < right) {
-        const sum = sortedNums[left] + sortedNums[right];
-        if (sum === target) {
-            const res: Array<number> = [];
-            let addedLeft = false;
-            let addedRight = false;
-            for (let i = 0; i < nums.length; i += 1) {
-                if (!addedLeft && nums[i] === sortedNums[left]) {
-                    addedLeft = true;
-                    res.push(i);
-                } else if (!addedRight && nums[i] === sortedNums[right]) {
-                    addedRight = true;
-                    res.push(i);
-                }
-
-                if (addedLeft && addedRight) {
-                    return res;
-                }
-            }
-        } else if (sum < target) {
-            left += 1;
-        } else {
-            right -= 1;
+    // O(N) if we don't find a match, otherwise always better than O(N/2)
+    for (let i = 0; i < nums.length; i += 1) {
+        const diff = target - nums[i];
+        const matches = values.get(diff);
+        // Worst case: O(N), usually O(1)
+        const index = matches?.find((idx) => idx !== i);
+        if (index) {
+            return [i, index];
         }
     }
     return [];
